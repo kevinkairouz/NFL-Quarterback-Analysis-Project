1da40f29d4ae83500d2ab4b0f31a21846f98a3aa
kevinkairouz <KM.Kairouz@gmail.com>
2025-11-20 22:23:38 -0500

    added legend to pie chart

---
+++ totalTouchdownsqb.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt 
+
+quarterbacks = ["Patrick Mahomes", "Lamar Jackson", "Josh Allen", "Aaron Rodgers"]
+plt.title("Touchdowns thrown from 2020 - 2024")  
+
+
+mahomesTouchdowns = [38, 37, 41, 27, 26] 
+lamarTouchdowns = [26, 16, 17, 24, 41] 
+allenTouchdowns = [37, 36, 35, 29, 28] 
+rodgersTouchdowns = [48, 37, 26, 0, 28]  
+
+totalTouchdownsthrown = [sum(mahomesTouchdowns), sum(lamarTouchdowns), sum(allenTouchdowns), sum(rodgersTouchdowns)] 
+
+plt.pie(totalTouchdownsthrown, colors=["red", "purple", "blue", "green"])  
+plt.legend(["Patrick Mahomes", "Lamar Jackson", "Josh Allen", "Aaron Rodgers"])
+plt.show()
